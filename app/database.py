081d9f09d43dbac534ca6bfde9a793862eda4882
Lorenzo Young <lorenzo.ivan.young@gmail.com>
2026-09-03 06:06:51 -0600

    Build: Add Iterator to db session

--- app/database.py
+++ app/database.py
@@ -1,6 +1,8 @@
-from sqlalchemy import create_engine
 from collections.abc import Iterator
-from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
+
 from app.config import get_settings
 
 settings = get_settings()
